cafd7af36de024acc6646e5434ef6fc3d2898f1c
jiries khazen <j.kzn91.s@hotmail.com>
2020-10-26 16:15:24 +0200

    add the functions backend

---
+++ functions.js
@@ -0,0 +1,36 @@
+//add item at the end of the array
+function addTask(array, task) {
+    array.push(task);
+}
+return array;
+
+//delete item from the array
+function deleteTask(array, index) {
+    if (typeof index === 'string' && index % 1 === 0) {
+        if (index < array.length && index >= 0) {
+            array.splice(index, 1);
+        }
+        else {
+            return "index not found";
+        }
+    }else{
+        return "index not a number";
+    }
+    return array;
+}
+
+
+//comlete items in array
+function completeTask(array, index) {
+    if (typeof index === 'string' && index % 1 === 0) {
+        if (index < array.length && index >= 0) {
+            array[index].isCompleted == true;
+        } else {
+            return "index not found";
+        }
+    }else{
+        return "index not a number";
+    }
+    return array;
+}
+
